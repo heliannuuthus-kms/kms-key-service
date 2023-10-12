e2b64f9147dd297310d897cb84f8f71f7e9ec57d
tangbo <tangbo@lixiang.com>
2023-10-12 10:18:04 +0800

    format

--- src/controller/secret_controller.rs
+++ src/controller/secret_controller.rs
@@ -3,9 +3,6 @@ use actix_web::{post, web::Json, Responder};
 use crate::{common::errors::Result, pojo::form::secret::SecretCreateForm};
 
 #[post("")]
-pub async fn create_secret(
-    Json(form): Json<SecretCreateForm>,
-) -> Result<impl Responder> {
-    
-  Ok("")
+pub async fn create_secret(Json(_form): Json<SecretCreateForm>) -> Result<impl Responder> {
+    Ok("")
 }
