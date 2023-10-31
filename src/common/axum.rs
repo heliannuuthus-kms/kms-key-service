5dc16656375a1a0567724aefecd3013d36faab7b
tangbo <tangbo@lixiang.com>
2023-10-31 10:33:23 +0800

    add missing file

---
+++ src/common/axum.rs
@@ -0,0 +1,88 @@
+use axum::{
+    extract::{
+        rejection::{FormRejection, JsonRejection, QueryRejection},
+        FromRequest,
+    },
+    response::IntoResponse,
+};
+use chrono::{DateTime, FixedOffset, Local};
+use http::StatusCode;
+use serde::Serialize;
+use serde_json::json;
+
+#[derive(FromRequest)]
+#[from_request(via(axum::Json), rejection(ErrorResponse))]
+pub struct Json<T>(pub T);
+
+#[derive(FromRequest)]
+#[from_request(via(axum::Form), rejection(ErrorResponse))]
+pub struct Form<T>(pub T);
+
+#[derive(FromRequest)]
+#[from_request(via(axum::extract::Query), rejection(ErrorResponse))]
+pub struct Query<T>(pub T);
+
+impl<T: Serialize> IntoResponse for Json<T> {
+    fn into_response(self) -> axum::response::Response {
+        let Self(value) = self;
+        axum::Json(value).into_response()
+    }
+}
+
+impl<T: Serialize> IntoResponse for Form<T> {
+    fn into_response(self) -> axum::response::Response {
+        let Self(value) = self;
+        axum::Json(value).into_response()
+    }
+}
+
+#[derive(Debug)]
+pub struct ErrorResponse {
+    code: StatusCode,
+    msg: String,
+    timestamp: DateTime<FixedOffset>,
+}
+
+impl From<JsonRejection> for ErrorResponse {
+    fn from(rejection: JsonRejection) -> Self {
+        Self {
+            code: rejection.status(),
+            msg: rejection.body_text(),
+            timestamp: Local::now().fixed_offset(),
+        }
+    }
+}
+
+impl From<FormRejection> for ErrorResponse {
+    fn from(rejection: FormRejection) -> Self {
+        Self {
+            code: rejection.status(),
+            msg: rejection.body_text(),
+            timestamp: Local::now().fixed_offset(),
+        }
+    }
+}
+
+impl From<QueryRejection> for ErrorResponse {
+    fn from(rejection: QueryRejection) -> Self {
+        Self {
+            code: rejection.status(),
+            msg: rejection.body_text(),
+            timestamp: Local::now().fixed_offset(),
+        }
+    }
+}
+// We implement `IntoResponse` so `ApiError` can be used as a response
+impl IntoResponse for ErrorResponse {
+    fn into_response(self) -> axum::response::Response {
+        (
+            self.code,
+            axum::Json(json!({
+                "code": self.code.as_u16(),
+                "msg": self.msg,
+                "timestamp": self.timestamp
+            })),
+        )
+            .into_response()
+    }
+}
